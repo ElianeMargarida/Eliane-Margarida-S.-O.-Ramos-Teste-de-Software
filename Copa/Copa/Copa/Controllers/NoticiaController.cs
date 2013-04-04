075bfc317bb37f1dc7e7101ca4439188b8a49677
Eliane Margarida <eliane.margarida@gmail.com>
2013-04-04 10:14:04 -0300

    Revert "Revert "Signed-off-by: ElianeMargarida <seu_email@email.com>""
    This reverts commit 4e3b66223a91f6dc8ca7ba16aa21130fb5cb9a9d.

---
+++ Copa/Copa/Copa/Controllers/NoticiaController.cs
@@ -0,0 +1,130 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Copa.Models;
+
+namespace Copa.Controllers
+{
+    public class NoticiaController : Controller
+    {
+        private GerenciarNoticiaEntities db = new GerenciarNoticiaEntities();
+
+        //
+        // GET: /Noticia/
+
+        public ActionResult Index()
+        {
+            var noticia = db.noticia.Include("comentario").Include("usuario");
+            return View(noticia.ToList());
+        }
+
+        //
+        // GET: /Noticia/Details/5
+
+        public ActionResult Details(int id = 0)
+        {
+            noticia noticia = db.noticia.Single(n => n.idnoticia == id);
+            if (noticia == null)
+            {
+                return HttpNotFound();
+            }
+            return View(noticia);
+        }
+
+        //
+        // GET: /Noticia/Create
+
+        public ActionResult Create()
+        {
+            ViewBag.idcomentario = new SelectList(db.comentario, "idcomentario", "conteudo");
+            ViewBag.idusuario = new SelectList(db.usuario, "idusuario", "nome");
+            return View();
+        }
+
+        //
+        // POST: /Noticia/Create
+
+        [HttpPost]
+        public ActionResult Create(noticia noticia)
+        {
+            if (ModelState.IsValid)
+            {
+                db.noticia.AddObject(noticia);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            ViewBag.idcomentario = new SelectList(db.comentario, "idcomentario", "conteudo", noticia.idcomentario);
+            ViewBag.idusuario = new SelectList(db.usuario, "idusuario", "nome", noticia.idusuario);
+            return View(noticia);
+        }
+
+        //
+        // GET: /Noticia/Edit/5
+
+        public ActionResult Edit(int id = 0)
+        {
+            noticia noticia = db.noticia.Single(n => n.idnoticia == id);
+            if (noticia == null)
+            {
+                return HttpNotFound();
+            }
+            ViewBag.idcomentario = new SelectList(db.comentario, "idcomentario", "conteudo", noticia.idcomentario);
+            ViewBag.idusuario = new SelectList(db.usuario, "idusuario", "nome", noticia.idusuario);
+            return View(noticia);
+        }
+
+        //
+        // POST: /Noticia/Edit/5
+
+        [HttpPost]
+        public ActionResult Edit(noticia noticia)
+        {
+            if (ModelState.IsValid)
+            {
+                db.noticia.Attach(noticia);
+                db.ObjectStateManager.ChangeObjectState(noticia, EntityState.Modified);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            ViewBag.idcomentario = new SelectList(db.comentario, "idcomentario", "conteudo", noticia.idcomentario);
+            ViewBag.idusuario = new SelectList(db.usuario, "idusuario", "nome", noticia.idusuario);
+            return View(noticia);
+        }
+
+        //
+        // GET: /Noticia/Delete/5
+
+        public ActionResult Delete(int id = 0)
+        {
+            noticia noticia = db.noticia.Single(n => n.idnoticia == id);
+            if (noticia == null)
+            {
+                return HttpNotFound();
+            }
+            return View(noticia);
+        }
+
+        //
+        // POST: /Noticia/Delete/5
+
+        [HttpPost, ActionName("Delete")]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            noticia noticia = db.noticia.Single(n => n.idnoticia == id);
+            db.noticia.DeleteObject(noticia);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            db.Dispose();
+            base.Dispose(disposing);
+        }
+    }
+}
